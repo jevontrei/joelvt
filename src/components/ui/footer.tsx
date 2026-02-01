20e421dd05ec4d4ce42f8c0b970740c5a16ad83c
jevontrei <160815182+jevontrei@users.noreply.github.com>
2026-02-01 14:49:58 +1000

    add more icons; rename repo

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -14,6 +14,9 @@ export default function Footer() {
           href="https://github.com/jevontrei/"
           target="_blank"
           rel="noopener noreferrer"
+          // TODO: fix this color/hover stuff
+          // NOTE: i added fill="currentColor" in the .svg itself
+          className="text-gray-200 hover:grayscale-text-black transition-colors"
         >
           <Image src="/github.svg" alt="GitHub logo" width={24} height={24} />
         </Link>
@@ -21,6 +24,7 @@ export default function Footer() {
           href="https://www.linkedin.com/in/joel-von-treifeldt/"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:grayscale-50"
         >
           <Image
             src="/linkedin-logo-svgrepo-com.svg"
