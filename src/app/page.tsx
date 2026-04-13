c21ca1035e295e4855dd6e576f0f8ee6779dc093
jevontrei <160815182+jevontrei@users.noreply.github.com>
2026-04-13 20:44:15 +1000

    update copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
       <h2 className="text-6xl mt-8 mb-4 mx-4">Joel von Treifeldt</h2>
       <p>Welcome to my corner of the internet.</p>
       <p>
-        This is where I practise Next.js, maintain a movies database and share
-        some photos.
+        A full-stack Next.js project with a movies database (TMDB API) and
+        travel photos (AWS SDK).
       </p>
     </div>
   );
