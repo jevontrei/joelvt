f52c0e98733338feb48b8d8bb94dc1cec68b8ace
Joel von Treifeldt <jevontrei@gmail.com>
2026-04-26 09:04:31 +1000

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // default export -- can import with any name
 export default function Page() {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="px-8 max-w-lg flex flex-col items-center gap-4">
       <h2 className="text-4xl mt-8 mb-4 mx-4">Joel von Treifeldt</h2>
-      <p>Welcome to my corner of the internet.</p>
-      <p>
+      <p className="text-center">Welcome to my corner of the internet.</p>
+      <p className="text-center">
         A full-stack Next.js project with a movies database (TMDb API), travel
         photos (AWS SDK), a reading list, and a music language database (Spotify
         Web API).
